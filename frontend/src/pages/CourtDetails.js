045d4d9d5d21a0648f189042e06664d2404e8786
alexdumi0106 <alexdumi010603@gmail.com>
2025-03-31 11:40:47 +0300

    Structura frontend aplicatie

---
+++ frontend/src/pages/CourtDetails.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <h1>CourtDetails Page</h1>
+      {/* Aici vei adăuga lista terenurilor */}
+    </div>
+  );
+};
+
+export default Home;
