d14be28d8dc5a42f7308ee65ecadd8f87008c7c7
Dariu Gornic <dariu.gornic@student.upt.ro>
2025-04-28 00:24:56 +0300

    Small change in the query of the database for courtsModel.js

--- backend/src/models/courtsModel.js
+++ backend/src/models/courtsModel.js
@@ -8,8 +8,15 @@ exports.fetchAllCourts = async () => {
 
 exports.getCourtWithComplex = async (courtId) => {
     const result = await pool.query(
-      `SELECT courts.id, courts.name, sports_complexes.name AS complex_name
-       FROM courts
+      `SELECT
+        courts.id,
+        courts.name,
+        courts.description,
+        courts.image_url,
+        sports_complexes.name AS complex_name,
+        sports_complexes.location,
+        sports_complexes.image_url AS complex_image 
+      FROM courts
        JOIN sports_complexes ON courts.sports_complex_id = sports_complexes.id
        WHERE courts.id = $1`,
       [courtId]
